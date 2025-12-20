e35b0f0308943e137533ab0d28b26ac156885b9c
igonik2003 <igonik2003@gmail.com>
2025-12-20 22:19:15 +0100

    Created DS1 sensor and simulator

---
+++ components/DS1.py
@@ -0,0 +1,19 @@
+#from sensors.DS1 import run_ds1_loop
+from simulators.DS1 import run_ds1_simulator
+import threading
+import time     
+
+def run_ds1(settings, threads, stop_event):
+        if settings['simulated']==True:
+            ds1_thread = threading.Thread(target = run_ds1_simulator, args=(2,stop_event))
+            print("Ds1 sumilator started")
+            ds1_thread.start()
+            threads.append(ds1_thread)
+        else:
+            """
+            ds1_thread = threading.Thread(target=run_ds1_loop, args=(2,stop_event))
+            print("Ds1 loop started")    
+            ds1_thread.start()
+            threads.append(ds1_thread)
+            """
+            print("Real sensor implementation.")                     
